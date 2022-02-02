2b39b2d8b8ce0555d76ea52d2c60ae0f7fb3b793
Rwixn <90738240+Rwixn@users.noreply.github.com>
2022-02-02 18:51:23 +0330

    Add files via upload

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+$admins = [147787696];
+$listplugins = [
+  "ping",
+  "help",
+   "plus",
+   "numb",
+   "enemynam",
+  "enemy",
+  "option"
+];
+$cplug = count($listplugins) - 1;
+for($n=0; $n<=$cplug; $n++) {
+  $pluginlist = "plugins/".$listplugins[$n].".php";
+  include($pluginlist);
+}
+$data = json_decode(file_get_contents("data.json"),true);
+$markread = $data["data"]["markread"];
+$typing = $data["data"]["typing"];
+$contacts = $data["data"]["contacts"];
+$link = $data["data"]["link"];
+unlink("MadelineProto.log");
